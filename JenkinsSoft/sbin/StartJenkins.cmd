85e6f200ad9351f728e094dd7f364626846eef04
joel.denagbe <joel.denagbe@gmail.com>
2015-08-23 17:49:58 +0200

    Utilisation de jenkins.war 1625

--- JenkinsSoft/sbin/StartJenkins.cmd
+++ JenkinsSoft/sbin/StartJenkins.cmd
@@ -1,1 +1,1 @@
-start java -jar %JENKINS_BASE%\war\jenkins.1624.war --httpPort=8080 --controlPort=8001 --logfile=%JENKINS_BASE%\logs\jenkins.log
+start java -jar %JENKINS_BASE%\war\jenkins.1625.war --httpPort=8080 --controlPort=8001 --logfile=%JENKINS_BASE%\logs\jenkins.log
